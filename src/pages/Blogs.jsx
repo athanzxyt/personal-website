e4c216b6bcca907cf07334a4943a1b937ad55e64
Athan Zhang <athanzxyt@gmail.com>
2024-07-01 00:25:33 -0400

    ADD: Blog Placeholder

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -5,7 +5,16 @@ import React from 'react';
 function Blogs() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Blogs</h1>
+      <div className="text-zinc-500 pb-8">
+        <p>
+          This section contains literally anything. From favorite quotes
+          to random thoughts, it's a place where I can share my ideas and
+          experiences with the world.
+        </p>
+      </div>
+      <h1 className='pt-4 font-serif'>
+          Coming Soon :3
+        </h1>
     </div>
   )
 }
