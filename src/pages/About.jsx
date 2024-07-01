e2859262652f630a2bfefaba2d61785d21d878bd
Athan Zhang <athanzxyt@gmail.com>
2024-06-30 20:05:22 -0400

    ADD: About Bio

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,20 @@ import React from 'react';
 function About() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">About</h1>
+      <p>
+        I'm currently an undergraduate at Princeton in an accelerated
+        track studying Computer Science. Currently I'm helping build
+        backend machine learning pipelines and advising with strategy at
+        <span> Vytal</span>
+        , a startup I helped start with a few friends back in high school.
+        <br />
+        <br />
+        On campus, I'm primarily involved with pursuing research in the 
+        field of Human-Computer Interaction and working at
+        <span> Prospect Student Ventures</span> (Princeton's student-run VC),
+        as the Director of Founder Support. In my free time, I enjoying training for triathlons (swim, bike, run),
+        lifting, reading, and following the latest trends in tech.
+      </p>
     </div>
   )
 }
