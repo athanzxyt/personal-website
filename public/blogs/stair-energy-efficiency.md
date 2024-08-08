1cbbc848230e1ea8f53a58ba0b36014939b2328a
Athan Zhang <athanzxyt@gmail.com>
2024-08-08 10:56:09 -0400

    Initial Recreation

--- public/blogs/stair-energy-efficiency.md
+++
@@ -1,8 +0,0 @@
----
-title: Are Stairs More Energy Efficient Than Elevators?
-date: 8/1/2024
----
-
-When comparing stairs and elevators in terms of energy efficiency, stairs are significantly more energy-efficient. Using stairs requires no electrical energy, relying solely on human physical effort. This not only conserves energy but also provides health benefits such as improved cardiovascular fitness and muscle strength.
-
-On the other hand, elevators consume electrical energy to operate. While modern elevators are designed to be more energy-efficient, they still require a considerable amount of energy, especially in high-rise buildings with frequent usage. Additionally, elevators need maintenance and occasional repairs, which adds to their overall energy footprint.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      fontFamily: {
-        'sans': ['Archivo', 'sans-serif'],
-        'serif': ['Lora', 'serif'],
-      },
-      colors: {
-        'primary': '#0A0A0A',
-        'secondary': '#0D8746',
-        'tertiary': '#FDFDFD'
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
